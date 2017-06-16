517993c1a9af9abfd0b1dbee069d4df0d4f41b69
SE <jules.tenajeros@gmail.com>
2017-06-16 17:31:05 +0800

    2nd

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ var app = express();
 //set port
 app.set('port', (process.env.PORT || 5000));
 //use static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'TenajerosA1_files')));
 //express routes
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'TenajerosA1.html'));
 });
-
 //express server listen
 var server = app.listen(app.get('port'), function(){
   console.log('Server listening on port ',app.get('port'));
